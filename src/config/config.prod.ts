82af666bb16fb73c55bb0d040e265f08d52be518
wewelove88 <wewelove88@gmail.com>
2023-04-12 17:48:02 +0800

    修改配置

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -2,14 +2,19 @@ import { MidwayConfig } from '@midwayjs/core';
 
 export default {
   midwayLogger: {
+    default: {
+      dir: '/home/app/logs', // 日志根目录
+      maxSize: '10m', // 日志切割
+      maxFiles: '7d', // 日志清理
+    },
     clients: {
       coreLogger: {
-        level: 'info',
-        consoleLevel: 'info',
+        level: 'warn',
+        consoleLevel: 'warn',
       },
       appLogger: {
-        level: 'info',
-        consoleLevel: 'info',
+        level: 'warn',
+        consoleLevel: 'warn',
       },
     },
   },
